fd9bf6148c3c9737b43ead2632e589a59f7a994f
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2026-05-02 21:08:22 +0200

    create: digital footprint section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Header from "@/app/components/Header";
+import DigitalFootprint from "@/app/components/DigitalFootprint";
 
 export default function Home() {
 	return (
-		< Header />
+		<>
+			<Header />
+			<DigitalFootprint />
+		</>
 	);
 }
